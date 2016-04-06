8c64a5074f8da6c52a0272d64b5611af3827f63f
Drew Meyers <drew.meyers@colorado.edu>
2016-04-06 00:22:06 +0000

    Added files via upload

---
+++ dashboard.php
@@ -0,0 +1,16 @@
+<?php include("auth.php"); //include auth.php file on all secure pages ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Data Download and Visualization</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="form">
+<p>Pod data download to be added here.</p>
+<p><a href="CSVimport.php">Upload Raw Pod Data</a></p>
+<a href="logout.php">Logout</a>
+</div>
+</body>
+</html>
